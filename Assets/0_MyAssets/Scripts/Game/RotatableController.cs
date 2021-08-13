a05ca4c020994340462f9a4d0a99eea49e6756a4
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2021-08-13 19:10:21 +0900

    回転方法変更

--- Assets/0_MyAssets/Scripts/Game/RotatableController.cs
+++ Assets/0_MyAssets/Scripts/Game/RotatableController.cs
@@ -23,4 +23,13 @@ public class RotatableController : MonoBehaviour
         tween = transform.DORotate(angle, 0.4f);
         counter *= -1;
     }
+
+    public void OnDrag()
+    {
+        Vector3 screenPos = Input.mousePosition;
+        screenPos.z = -Camera.main.transform.position.z;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        worldPos.x = Mathf.Clamp(worldPos.x, -3.4f, 3.4f);
+        transform.up = worldPos - transform.position;
+    }
 }
