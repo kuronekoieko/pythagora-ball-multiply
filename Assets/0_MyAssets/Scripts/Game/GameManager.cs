ef2eb2b757b519db1c532272a03f5e871fbd17a3
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2021-08-23 13:44:23 +0900

    UIシーンに遷移

--- Assets/0_MyAssets/Scripts/Game/GameManager.cs
+++ Assets/0_MyAssets/Scripts/Game/GameManager.cs
@@ -5,18 +5,16 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    private void Awake()
-    {
-
-    }
-
-    private void Start()
-    {
-
-    }
-
     private void Update()
     {
-
+        if (Variables.screenState != ScreenState.Game) return;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Variables.screenState = ScreenState.Failed;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Variables.screenState = ScreenState.Clear;
+        }
     }
 }
